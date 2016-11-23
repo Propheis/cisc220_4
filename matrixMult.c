344ebc84a4490e78bd152db69b476fd483cb4377
Kevin Zuern <kevin@kevinzuern.com>
2016-11-23 16:04:49 -0500

    Finish matrixMult

--- matrixMult.c
+++ matrixMult.c
@@ -2,18 +2,42 @@
 # include <stdlib.h>
 # include <string.h>
 
-#define MAX_INPUT_LENGTH 50
+#define MAX_INPUT_LENGTH 1000
+
+char isInteger(const char *str) {
+  char *endptr;
+  strtol(str, &endptr, 10);
+  if (str == endptr)
+    return 0;
+  return 1;
+}
+
+// Prints the message, than the matrix in a nice format
+void printMatrix(const char *message, int num_rows, int num_cols, int matrix[]) {
+  printf("%s\n", message);
+
+  for (int i = 0; i < num_rows; i++) {
+    for (int j = 0; j < num_cols; j++)
+      printf("%d\t", matrix[ (i * num_cols) + j ]);
+    printf("\n");
+  }
+  
+  printf("\n");
+}
 
 // Parses a space delimited string of ints into an array of ints
-void stringToIntArray(const char* str, int *intArr, int arrSize) {
-  char *input = strdup(str);
+void stringToIntArray(char* input, int intArr[], int arrSize) {
   char *token;
   token = strtok(input, " ");
   int i = 0;
-  while (token != NULL) {
+  while (token != NULL && i < arrSize) {
+    if (!isInteger(token)) {
+      printf("%s\n", "Invalid Input");
+      exit(-1);
+    }
     intArr[i] = atoi(token);
-    token = strtok(NULL, " ");
     i++;
+    token = strtok(NULL, " ");
   }
 }
 
@@ -22,7 +46,7 @@ void getMatrixDimensions(int matrixNumber, int *dimensionX, int *dimensionY) {
   printf("Insert matrix %d dimensions separated by a space: ", matrixNumber);
   
   char input[MAX_INPUT_LENGTH];
-  scanf("%s", &input);
+  fgets(input, MAX_INPUT_LENGTH, stdin);
 
   // Get the dimensions from stdin
   int data[3];
@@ -32,33 +56,21 @@ void getMatrixDimensions(int matrixNumber, int *dimensionX, int *dimensionY) {
   *dimensionY = data[1];
 }
 
+// Returns a matrix. matrixNumber is printed into the prompt
+void getMatrix(int matrixNumber, int num_rows, int num_cols, int *matrix) {
+  printf("Input matrix %d elements separated by spaces:", matrixNumber);
+  
+  char input[MAX_INPUT_LENGTH];
+  fgets(input, MAX_INPUT_LENGTH, stdin);
+
+  stringToIntArray(input, matrix, num_rows * num_cols);
+}
+
 // Takes a 2D dimension vector of each matrix and returns 0 if you can multiply safely
 char canMultiply(int m1y, int m2x) {
   if (m1y == m2x)
     return 0;
   return 1;
-}
-
-// Returns a matrix. matrixNumber is printed into the prompt
-void getMatrix(int matrixNumber, size_t num_rows, size_t num_cols, int *matrix) {
-  printf("Input matrix %d elements separated by spaces:", matrixNumber);
-  char input[MAX_INPUT_LENGTH];
-  scanf("%s", &input);
-
-  stringToIntArray(strdup(input), matrix, num_rows*num_cols);
-}
-
-// Prints the message, than the matrix in a nice format
-void printMatrix(const char **message, int num_rows, int num_cols, int matrix[]) {
-  printf("%s\n", *message);
-
-  for (int i = 0; i < num_rows; i++) {
-    for (int j = 0; j < num_cols; j++)
-      printf("%d\t", matrix[ (i * num_cols) + j ]);
-    printf("\n");
-  }
-  
-  printf("\n");
 }
 
 // Perform a matrix multiplication
@@ -70,7 +82,7 @@ void matrixMult(int m1X, int m1Y, int m2X, int m2Y, int *m1, int *m2, int *mResu
     // By Columns
     for (int j = 0; j < m2Y; j++) {
   
-      int result;
+      int result = 0;
       printf("%s [%d, %d] = ", "Element", i+1, j+1);
       
       // Multiply m1's row by m2's column
@@ -94,12 +106,7 @@ void matrixMult(int m1X, int m1Y, int m2X, int m2Y, int *m1, int *m2, int *mResu
   printf("\n");
 }
 
-
 int main(int argc, char const *argv[]) {
-  const char* msg1 = "Matrix 1: ";
-  const char* msg2 = "Matrix 2: ";
-  const char* msg3 = "Final output: ";
-  const char* msg4 = "M1 * M2 * M3: ";
   // Stores the dimensions of each of the matrices
   int m1x, m1y
     , m2x, m2y
@@ -107,67 +114,52 @@ int main(int argc, char const *argv[]) {
     , resultx, resulty;
 
   getMatrixDimensions(1, &m1x, &m1y);
+  getMatrixDimensions(2, &m2x, &m2y);
 
-  printf("%d %d\n", m1x, m1y);
+  while (canMultiply(m1y, m2x) != 0) {
+    fputs("Dimensions mismatch\n", stderr);
+    getMatrixDimensions(2, &m2x, &m2y);
+  }
 
-  // getMatrixDimensions(2, &m2x, &m2y);
+  // Initialize resultx and resulty
+  resultx = m1x;
+  resulty = m2y;
 
-  // while (canMultiply(m1y, m2x) != 0) {
-  //   fputs("Dimensions mismatch\n", stderr);
-  //   getMatrixDimensions(2, &m2x, &m2y);
-  // }
+  // // Initialize the matrices
+  int m1[ m1x * m1y ]
+    , m2[ m2x * m2y ]
+    , result[ resultx * resulty ];
 
-  // // Initialize resultx and resulty
-  // resultx = m1x;
-  // resulty = m2y;
+  getMatrix(1, m1x, m1y, m1);
+  getMatrix(2, m2x, m2y, m2);
 
-  // // // Initialize the matrices
-  // int m1[m1x][m1y]
-  // ,   m2[m2x][m2y]
-  // ,   m3[m3x][m3y]
-  // ,   result[resultx][resulty];
+  printMatrix("Matrix 1:",  m1x, m1y, m1);
+  printMatrix("Matrix 2:",  m2x, m2y, m2);
 
-  // getMatrix(1, m1x, m1y, &m1);
-  // getMatrix(2, m2x, m2y, &m2);
+  matrixMult(m1x, m1y, m2x, m2y, m1, m2, result);
 
-  // printMatrix("Matrix 1:",  m1x, m1y, &m1);
-  // printMatrix("Matrix 2:",  m2x, m2y, &m2);
-
-  // matrixMult(m1x, m1y, &m1, m2x, m2y, &m2, &result);
-
-  // printMatrix("Final output:", result, resultx, resulty);
-
-  // do {
-  //   fputs("Dimensions mismatch", stderr);
-  //   getMatrixDimensions(3, &m3x, &m3y);
-  // } while (canMultiply(resulty, m3x) != 0);
-
-  // getMatrix(3, m3);
-
-  // int finalMatrix = matrixMult(result, m3);
-
-  // printMatrix("m1 * m2 * m3", finalMatrix);
-
-  // return 0;
-}
-
-// int main(int argc, char const *argv[])
-// {
-//   const char* msg1 = "Matrix 1: ";
-//   const char* msg2 = "Matrix 2: ";
-//   const char* msg3 = "Final output: ";
-//   // 2x3 matrix
-//   int m1[6] = { 1, 2, 3, 4, 5, 6 };
-//   // 3x4 matrix
-//   int m2[12] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120 };
+  printMatrix("Final output:", resultx, resulty, result);
   
-//   printMatrix(&msg1, 2, 3, m1);
-//   printMatrix(&msg2, 3, 4, m2);
+  // Get dimensions for the new matrix
+  getMatrixDimensions(3, &m3x, &m3y);
+  while (canMultiply(resulty, m3x) != 0) {
+    fputs("Dimensions mismatch\n", stderr);
+    getMatrixDimensions(3, &m3x, &m3y);
+  }
 
-//   int m3[2*4];
-//   matrixMult(2, 3, 3, 4, m1, m2, m3);
+  // Initialize matrix
+  int m3[m3x * m3y];
+  getMatrix(3, m3x, m3y, m3);
+  printf("\n");
+  printMatrix("Matrix 3:", m3x, m3y, m3);
 
-//   printMatrix(&msg3, 2, 4, m3);
+  printf("\n");
 
-//   return 0;
-// }
+  // Perform the multiplication
+  int finalMatrix[resultx * m3y];
+  matrixMult(resultx, resulty, m3x, m3y, result, m3, finalMatrix);
+
+  printMatrix("M1 * M2 * M3: ", resultx, m3y, finalMatrix);
+
+  return 0;
+}
